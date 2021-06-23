fbb287dda89df5863c081efde48779a53b004589
Greg <Gdixon5125@gmail.com>
2021-06-23 12:54:55 -0400

    b

--- XHR/XHR-master/js/xmlhttp.js
+++ XHR/XHR-master/js/xmlhttp.js
@@ -1,2 +1,15 @@
 // handle button clicks
-// when the button is clicked, load content into #page_content from the associated /file/ resource
+document.getElementById("page1").onclick = function() {myFunction()};document.getElementById("page2").onclick = function() {myFunction()};document.getElementById("page3").onclick = function() {myFunction()};
+// when the button is clicked, load content into #page_content from the as(sociated /file/ resource
+function myFunction() {
+    document.getElementById("page1").addEventListener('click', function(e)) {
+
+    };
+    document.getElementById("page2").addEventListener('click', function(e)) {
+
+    }
+    document.getElementById("page1").addEventListener
+  ('click', function(e)) {
+
+    }
+}
