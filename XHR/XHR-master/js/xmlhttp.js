c5f0eb95c1e757cafbac8f95ad3c0d9fd9022816
Greg <Gdixon5125@gmail.com>
2021-06-23 15:23:48 -0400

    nj

--- XHR/XHR-master/js/xmlhttp.js
+++ XHR/XHR-master/js/xmlhttp.js
@@ -1,15 +1,40 @@
 // handle button clicks
-document.getElementById("page1").onclick = function() {myFunction()};document.getElementById("page2").onclick = function() {myFunction()};document.getElementById("page3").onclick = function() {myFunction()};
-// when the button is clicked, load content into #page_content from the as(sociated /file/ resource
+document.getElementById("page1").onclick = function() {myFunction()};
+document.getElementById("page2").onclick = function() {myFunction()};
+document.getElementById("page3").onclick = function() {myFunction()};
+
 function myFunction() {
     document.getElementById("page1").addEventListener('click', function(e)) {
-
+        let page1 = new XMLHttpRequest();
+        page1.onload = function() {
+           // this.responseText
+    };
+        page1.open('GET', 'page1.html');
+        page1.send();
     };
     document.getElementById("page2").addEventListener('click', function(e)) {
-
+        let page2 = new XMLHttpRequest();
+        page2.onload = function() {
+         // this.responseText
+    };
+        page2.open('GET', 'page2.html');
+        page2.send();
     }
-    document.getElementById("page1").addEventListener
-  ('click', function(e)) {
+    document.getElementById("page3").addEventListener('click', function(e)) {
+    let page3 = new XMLHttpRequest();
+        page3.onload = function() {
+        // this.responseText
+    };
+        page3.open('GET', 'page3.html');
+        page3.send();
 
-    }
 }
+// when the button is clicked, load content into #page_content from the as(sociated /file/ resource
+
+function reqListener () {
+    console.log(this.responseText);
+}
+
+
+
+
