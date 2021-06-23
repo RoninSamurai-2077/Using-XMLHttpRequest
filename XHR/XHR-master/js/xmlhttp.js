fbc9e8040f3e13160ae0ca19065ab3f5f1143ac8
Greg <Gdixon5125@gmail.com>
2021-06-23 17:24:12 -0400

    mmm

--- XHR/XHR-master/js/xmlhttp.js
+++ XHR/XHR-master/js/xmlhttp.js
@@ -3,27 +3,35 @@ document.getElementById("page1").onclick = function() {myFunction()};
 document.getElementById("page2").onclick = function() {myFunction()};
 document.getElementById("page3").onclick = function() {myFunction()};
 // when the button is clicked, load content into #page_content from the as(sociated /file/ resource
-function myFunction() {
+let pages = {
+    'page1': '/files/page1.html',
+    'page2': '/files/page2.html', 
+    'page3': '/files/page3.html',
+}
+    function myFunction() {
     document.getElementById("page1").addEventListener('click', function(e) {
-        var page1 = new XMLHttpRequest();
-        page1.onload = function() {
-           // this.responseText
+        let page = e.target.id;
+        let xhr = new XMLHttpRequest();
+        xhr.onload = function() {
+            document.getElementById('page_content').innerHTML=this.responseText;
     };
-        page1.open('GET', 'page1.html');
-        page1.send();
+    xhr.open('GET', 'page1.html');
+    xhr.send();
     
     document.getElementById("page2").addEventListener('click', function(e) {
-        var page2 = new XMLHttpRequest();
-        page2.onload = function() {
-         // this.responseText
-    };
-        page2.open('GET', 'page2.html');
-        page2.send();
+        let page = e.target.id;
+        let xhr = new XMLHttpRequest();
+        xhr.onload = function() {
+            document.getElementById('page_content').innerHTML=this.responseText;   
+        };
+        xhr.open('GET', 'page2.html');
+        xhr.send();
     
     document.getElementById("page3").addEventListener('click', function(e) {
-    var page3 = new XMLHttpRequest();
-        page3.onload = function() {
-        // this.responseText
-    };
-        page3.open('GET', 'page3.html');
-        page3.send();
+        let page = e.target.id;
+    let xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+            document.getElementById('page_content').innerHTML=this.responseText;
+        };
+        xhr.open('GET', 'page3.html');
+        xhr.send();
