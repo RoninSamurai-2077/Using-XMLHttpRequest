bd1b7cf8ffbd0503f0341f6fa30453a3f6a14b1b
Greg <Gdixon5125@gmail.com>
2021-06-28 16:34:38 -0400

    x

--- XHR/XHR-master/js/xmlhttp.js
+++ XHR/XHR-master/js/xmlhttp.js
@@ -1,61 +1,43 @@
 // handle button clicks
-document.getElementById("page1").onclick = function() {myFunction()};
-document.getElementById("page2").onclick = function() {myFunction()};
-document.getElementById("page3").onclick = function() {myFunction()};
 // when the button is clicked, load content into #page_content from the as(sociated /file/ resource
 let pages = {
     'page1': '/files/page1.html',
     'page2': '/files/page2.html', 
     'page3': '/files/page3.html',
 }
-    function myFunction() {
-    document.getElementById("page1").addEventListener('click', function(e) {
-        let page = e.target.id;
-        let xhr = new XMLHttpRequest();
-        xhr.onload = function() {
-            document.getElementById('page_content').innerHTML=this.responseText;
-    };
-    xhr.open('GET', 'page1.html');
-    xhr.send();
+var button = document.getElementById("action_buttons").children 
+ button = Array.prototype.slice.call(button) 
+ button.forEach(button => button.addEventListener("click", buttonClick ));
     
-    document.getElementById("page2").addEventListener('click', function(e) {
-        let page = e.target.id;
-        let xhr = new XMLHttpRequest();
-        xhr.onload = function() {
-            document.getElementById('page_content').innerHTML=this.responseText;   
-        };
-        xhr.open('GET', 'page2.html');
-        xhr.send();
-    
-    document.getElementById("page3").addEventListener('click', function(e) {
-        let page = e.target.id;
-    let xhr = new XMLHttpRequest();
-    xhr.onload = function() {
-            document.getElementById('page_content').innerHTML=this.responseText;
-        };
-        xhr.open('GET', 'page3.html');
-        xhr.send();
-        
-
         function buttonClick(event) {
-
+                console.log(event.target.id)
             let page = event.target.id;
-            console.log(page);
-
-            if (event.target.localName = 'button' ) {
-            let xhr = new XMLHttpRequest(); 
-            xhr.onload = loadPageContent;
-            xhr.open('GET', `/files/${page}.html`)
-            xhr.send();
-            }
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                // Typical action to be performed when the document is ready:
+                document.getElementById("page_content").innerHTML = xhttp.responseText;
+                }
+            };
+            xhttp.open("GET", '/files/'+page+'.html');
+            xhttp.send();
+            // console.log(page);
+    // let xhr = new XMLHttpRequest(); 
+    // xhr.onload = loadPageContent = function() {
+    //     if (this.readyState == 4 && this.status == 200) {
+    //     //Typical action to be performed when the document is ready:
+            //  xhr.open('GET', '/files/'+page+'.html');
+    //         xhr.send();
+//}
         }
             
-            document.addEventListener("DOMContentLoaded", function() // load pagel by default In page load
+    //         document.addEventListener("DOMContentLoaded", function() 
+    //         // load pagel by default In page load
             
-            // page1, page2, page3 click events
+    //         // page1, page2, page3 click events
             
-            document.getElementById('action_buttons').addEventListener('click', buttonClick);
+    //         //document.getElementById('action_buttons').addEventListener('click', buttonClick);
             
-            // document.getElementById('page1').addEventListener('click', buttonClick);
+    //         // document.getElementById('page1').addEventListener('click', buttonClick);
             
-            // document.getElementById('page2').addEventListener('click', buttonClick); // document.getElementById('page3').addEventListener('click', buttonClick);
+    //         // document.getElementById('page2').addEventListener('click', buttonClick); // document.getElementById('page3').addEventListener('click', buttonClick);
